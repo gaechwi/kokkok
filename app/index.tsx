e6e2b64f51674345ee89c4625fad86aca35e9876
jmj <wjsdncl2222@gmail.com>
2024-11-26 15:08:45 +0900

    Feat : 코드레빗 테스트

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useRouter } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { useEffect } from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const formatDate = (date: Date) => {
+    return date.toLocaleDateString("ko-KR", {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit",
+      timeZone: "Asia/Seoul",
+    });
+  };
+
   return (
     <View className="flex-1 items-center justify-center">
       <Text className="text-3xl font-bold">여러분들 진짜 대단해요</Text>
+      <Text className="text-3xl font-bold">{formatDate(new Date())}</Text>
       <StatusBar style="auto" />
     </View>
   );
