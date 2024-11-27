1c156212ccf3f5d9eb48cab8a8f954bc183a653e
CitrusSoda <dndhkaus1004@naver.com>
2024-11-27 23:59:35 +0900

    feat: 폰트 설정

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,12 @@
 import { StatusBar } from "expo-status-bar";
 // noUnusedImports 확인
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
 
 export default function App() {
 	return (
 		<View className="flex-1 items-center justify-center">
 			{/* useSortedClasses 확인 */}
-			<Text className="text-3xl font-bold">여러분들 진짜 대단해요</Text>
+			<Text className="font-pblack text-3xl">여러분들 진짜 대단해요</Text>
 			<StatusBar style="auto" />
 		</View>
 	);
