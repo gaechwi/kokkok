6089f0a6b992fff1165693f10ccb7795b0dc8765
CitrusSoda <dndhkaus1004@naver.com>
2024-11-27 22:34:12 +0900

    chore: biome 세팅 설정 추가

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
+import { StatusBar } from "expo-status-bar";
 // noUnusedImports 확인
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
-  return (
-    <View className="flex-1 items-center justify-center">
-      {/* useSortedClasses 확인 */}
-      <Text className="text-3xl font-bold">여러분들 진짜 대단해요</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<View className="flex-1 items-center justify-center">
+			{/* useSortedClasses 확인 */}
+			<Text className="text-3xl font-bold">여러분들 진짜 대단해요</Text>
+			<StatusBar style="auto" />
+		</View>
+	);
 }
