a57af229d5ce0dbfdc2351809c1d838eafe5b5de
heony704 <heony704@gmail.com>
2024-11-28 16:08:50 +0900

    feat: tailwind config에 white, black 컬러 추가

--- app/index.tsx
+++ app/index.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
 	return (
-		<View className="flex-1 items-center justify-center">
+		<View className="flex-1 text-white items-center justify-center">
 			{/* useSortedClasses 확인 */}
 			<Text className="text-3xl font-bold">여러분들 진짜 대단해요</Text>
 			<StatusBar style="auto" />
