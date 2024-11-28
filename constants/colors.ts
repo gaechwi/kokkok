26b3981aee3f07fcaef3931daf394ed4035a0fa5
정민재 <89517903+wjsdncl@users.noreply.github.com>
2024-11-28 23:17:27 +0900

    chore: 컬러 상수 설정 (#17)

---
+++ constants/colors.ts
@@ -0,0 +1,41 @@
+const primary = "var(--color-primary)";
+const secondaryYellow = "var(--color-secondary-yellow)";
+const secondaryRed = "var(--color-secondary-red)";
+const white = "var(--color-white)";
+const black = "var(--color-black)";
+const gray5 = "var(--color-gray-5)";
+const gray10 = "var(--color-gray-10)";
+const gray20 = "var(--color-gray-20)";
+const gray25 = "var(--color-gray-25)";
+const gray30 = "var(--color-gray-30)";
+const gray40 = "var(--color-gray-40)";
+const gray45 = "var(--color-gray-45)";
+const gray50 = "var(--color-gray-50)";
+const gray55 = "var(--color-gray-55)";
+const gray60 = "var(--color-gray-60)";
+const gray65 = "var(--color-gray-65)";
+const gray70 = "var(--color-gray-70)";
+const gray80 = "var(--color-gray-80)";
+const gray90 = "var(--color-gray-90)";
+
+export default {
+	primary,
+	secondaryYellow,
+	secondaryRed,
+	white,
+	black,
+	gray5,
+	gray10,
+	gray20,
+	gray25,
+	gray30,
+	gray40,
+	gray45,
+	gray50,
+	gray55,
+	gray60,
+	gray65,
+	gray70,
+	gray80,
+	gray90,
+};
