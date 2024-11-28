75fbaff4177d40e304db0023eb7aee237bed5b29
CitrusSoda <dndhkaus1004@naver.com>
2024-11-28 16:02:10 +0900

    feat: 패스워드 hide off 구현

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -9,8 +9,11 @@ import {
 
 import images from "../../constants/images";
 import icons from "../../constants/icons";
+import { useState } from "react";
 
 const SignIn = () => {
+	const [showPassword, setShowPassword] = useState(false);
+
 	return (
 		<View className="h-full bg-white">
 			<ScrollView>
@@ -28,11 +31,23 @@ const SignIn = () => {
 							keyboardType="email-address"
 							autoCapitalize="none"
 						/>
-						<TextInput
-							className="h-[58px] w-full rounded-[10px] border border-gray-20 px-4 focus:border-primary"
-							placeholder="비밀번호를 입력해주세요."
-							secureTextEntry
-						/>
+						<View className="w-full">
+							<TextInput
+								className="h-[58px] w-full rounded-[10px] border border-gray-20 px-4 focus:border-primary"
+								placeholder="비밀번호를 입력해주세요."
+								secureTextEntry={!showPassword}
+							/>
+							<TouchableOpacity
+								className="-translate-y-1/2 absolute top-1/2 right-4"
+								onPress={() => setShowPassword((prev) => !prev)}
+							>
+								{showPassword ? (
+									<icons.EyeOffIcon width={24} height={24} />
+								) : (
+									<icons.EyeIcon width={24} height={24} />
+								)}
+							</TouchableOpacity>
+						</View>
 					</View>
 
 					<TouchableOpacity className="mt-10 h-[62px] w-full items-center justify-center rounded-[10px] bg-primary">
--- constants/icons.ts
+++ constants/icons.ts
@@ -1,5 +1,7 @@
 import GoogleIcon from "../assets/icons/google.svg";
 import GithubIcon from "../assets/icons/github.svg";
 import KakaoIcon from "../assets/icons/kakao.svg";
+import EyeIcon from "../assets/icons/eye.svg";
+import EyeOffIcon from "../assets/icons/eye-off.svg";
 
-export default { GoogleIcon, GithubIcon, KakaoIcon };
+export default { GoogleIcon, GithubIcon, KakaoIcon, EyeIcon, EyeOffIcon };
