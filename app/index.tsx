d07c27ff21d164bd305d4e072033286bcf971735
정민재 <89517903+wjsdncl@users.noreply.github.com>
2024-11-28 16:18:01 +0900

    feat: 탭 레이아웃 구현 (#9)
    * feat: 탭 레이아웃 구현
    
    * feat: ios 헤더 설정 추가

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,5 @@
 import { StatusBar } from "expo-status-bar";
+import { Link } from "expo-router";
 // noUnusedImports 확인
 import { Text, View } from "react-native";
 
@@ -6,7 +7,11 @@ export default function App() {
 	return (
 		<View className="flex-1 items-center justify-center">
 			{/* useSortedClasses 확인 */}
-			<Text className="caption-1">여러분들 진짜 대단해요</Text>
+			<Text className="font-bold text-3xl">여러분들 진짜 대단해요</Text>
+			<View className="flex-row gap-2">
+				<Link href="/home">home</Link>
+				<Link href="/profile">profile</Link>
+			</View>
 			<StatusBar style="auto" />
 		</View>
 	);
