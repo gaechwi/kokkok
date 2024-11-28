1862cdcd7f12b908eb0090ac2ccc07b09dea4e47
jmj <wjsdncl2222@gmail.com>
2024-11-29 00:03:47 +0900

    chore: 색상 수정

--- constants/colors.ts
+++ constants/colors.ts
@@ -1,41 +1,25 @@
-const primary = "var(--color-primary)";
-const secondaryYellow = "var(--color-secondary-yellow)";
-const secondaryRed = "var(--color-secondary-red)";
-const white = "var(--color-white)";
-const black = "var(--color-black)";
-const gray5 = "var(--color-gray-5)";
-const gray10 = "var(--color-gray-10)";
-const gray20 = "var(--color-gray-20)";
-const gray25 = "var(--color-gray-25)";
-const gray30 = "var(--color-gray-30)";
-const gray40 = "var(--color-gray-40)";
-const gray45 = "var(--color-gray-45)";
-const gray50 = "var(--color-gray-50)";
-const gray55 = "var(--color-gray-55)";
-const gray60 = "var(--color-gray-60)";
-const gray65 = "var(--color-gray-65)";
-const gray70 = "var(--color-gray-70)";
-const gray80 = "var(--color-gray-80)";
-const gray90 = "var(--color-gray-90)";
-
-export default {
-	primary,
-	secondaryYellow,
-	secondaryRed,
-	white,
-	black,
-	gray5,
-	gray10,
-	gray20,
-	gray25,
-	gray30,
-	gray40,
-	gray45,
-	gray50,
-	gray55,
-	gray60,
-	gray65,
-	gray70,
-	gray80,
-	gray90,
-};
+export const colors = {
+	primary: "#8356F5",
+	secondary: {
+		yellow: "#FCEC5B",
+		red: "#FF7A7A",
+	},
+	white: "#FFFFFF",
+	black: "#2B2B2B",
+	gray: {
+		5: "#F8F8F8",
+		10: "#F2F2F2",
+		20: "#E1E1E1",
+		25: "#DDDDDD",
+		30: "#CECECE",
+		40: "#9A9A9A",
+		45: "#969696",
+		50: "#868686",
+		55: "#828282",
+		60: "#727272",
+		65: "#666666",
+		70: "#5D5D5D",
+		80: "#404040",
+		90: "#333333",
+	},
+} as const;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,35 +1,22 @@
+const { colors } = require("./constants/colors");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	// NOTE: Update this to include the paths to all of your component files.
 	content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
 	presets: [require("nativewind/preset")],
 	theme: {
-		colors: {
-			primary: "var(--color-primary)",
-			secondary: {
-				yellow: "var(--color-secondary-yellow)",
-				red: "var(--color-secondary-red)",
-			},
-			white: "var(--color-white)",
-			black: "var(--color-black)",
-			gray: {
-				5: "var(--color-gray-5)",
-				10: "var(--color-gray-10)",
-				20: "var(--color-gray-20)",
-				25: "var(--color-gray-25)",
-				30: "var(--color-gray-30)",
-				40: "var(--color-gray-40)",
-				45: "var(--color-gray-45)",
-				50: "var(--color-gray-50)",
-				55: "var(--color-gray-55)",
-				60: "var(--color-gray-60)",
-				65: "var(--color-gray-65)",
-				70: "var(--color-gray-70)",
-				80: "var(--color-gray-80)",
-				90: "var(--color-gray-90)",
-			},
-		},
 		extend: {
+			colors: {
+				primary: colors.primary,
+				secondary: {
+					yellow: colors.secondary.yellow,
+					red: colors.secondary.red,
+				},
+				white: colors.white,
+				black: colors.black,
+				gray: colors.gray,
+			},
 			fontFamily: {
 				pblack: ["Pretendard-Black"],
 				pbold: ["Pretendard-Bold"],
