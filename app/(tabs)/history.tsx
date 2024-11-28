991a4939bdc5a0b62351f36a56a06620f4f42a6c
heony704 <heony704@gmail.com>
2024-11-29 02:23:22 +0900

    feat: 타이포 적용

--- app/(tabs)/history.tsx
+++ app/(tabs)/history.tsx
@@ -8,14 +8,12 @@ export default function History() {
 	return (
 		<View className="flex-1 gap-5 bg-white px-7 py-6">
 			<View className="flex-row items-center">
-				<Text className="grow font-bold text-[23px] leading-normal">
+				<Text className="heading-1 grow">
 					11월 <Text className="text-primary">17</Text>일 운동 완료!
 				</Text>
 
 				<TouchableOpacity className="h-[36px] w-[85px] items-center justify-center rounded-lg border border-gray-25">
-					<Text className="font-normal text-[13px] text-gray-90 leading-normal">
-						쉬는 날 설정
-					</Text>
+					<Text className="body-5 text-gray-90">쉬는 날 설정</Text>
 				</TouchableOpacity>
 			</View>
 
@@ -24,7 +22,7 @@ export default function History() {
 					<TouchableOpacity>
 						<icons.ArrowLeftIcon width={20} height={20} color="#5D5D5D" />
 					</TouchableOpacity>
-					<Text className="font-bold text-xl leading-normal">11월</Text>
+					<Text className="heading-2">11월</Text>
 					<TouchableOpacity>
 						<icons.ArrowRightIcon width={20} height={20} color="#5D5D5D" />
 					</TouchableOpacity>
@@ -32,10 +30,7 @@ export default function History() {
 
 				<View className="mt-6 flex-row">
 					{["일", "월", "화", "수", "목", "금", "토"].map((day) => (
-						<Text
-							key={day}
-							className="flex-1 text-center font-medium text-[13px] text-gray-65"
-						>
+						<Text key={day} className="body-4 flex-1 text-center text-gray-65">
 							{day}
 						</Text>
 					))}
@@ -47,9 +42,7 @@ export default function History() {
 					keyExtractor={(item) => item.toString()}
 					renderItem={({ item }) => (
 						<View className="mt-2 flex w-[14.28%] items-center justify-center gap-2">
-							<Text className="font-medium text-[13px] text-gray-65">
-								{item}
-							</Text>
+							<Text className="body-4 text-gray-65">{item}</Text>
 							<icons.FaceDefaultIcon width={32} height={32} />
 						</View>
 					)}
@@ -58,29 +51,23 @@ export default function History() {
 			</View>
 
 			<View className="flex-row items-center rounded-[10px] border border-gray-25 px-[25px] py-[15px]">
-				<Text className="font-medium text-base leading-normal">
-					표정의 의미는?
-				</Text>
+				<Text className="title-4">표정의 의미는?</Text>
 				<View className="ml-auto flex-row gap-2">
-					<View className="w-[33px] items-center">
+					<View className="w-8 items-center">
 						<icons.FaceDefaultIcon width={24} height={24} />
-						<Text className="font-normal text-[10px] leading-normal">기본</Text>
+						<Text className="caption-3">기본</Text>
 					</View>
-					<View className="w-[33px] items-center">
+					<View className="w-8 items-center">
 						<icons.FaceDoneIcon width={24} height={24} />
-						<Text className="font-normal text-[10px] leading-normal">
-							운동함
-						</Text>
+						<Text className="caption-3">운동함</Text>
 					</View>
-					<View className="w-[33px] items-center">
+					<View className="w-8 items-center">
 						<icons.FaceNotDoneIcon width={24} height={24} />
-						<Text className="font-normal text-[10px] leading-normal">안함</Text>
+						<Text className="caption-3">안함</Text>
 					</View>
-					<View className="w-[33px] items-center">
+					<View className="w-8 items-center">
 						<icons.FaceRestIcon width={24} height={24} />
-						<Text className="font-normal text-[10px] leading-normal">
-							쉬는 날
-						</Text>
+						<Text className="caption-3">쉬는 날</Text>
 					</View>
 				</View>
 			</View>
