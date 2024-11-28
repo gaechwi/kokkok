6aeba3945650d54d241fa9ec9cbf561b5b6bde57
heony704 <heony704@gmail.com>
2024-11-29 00:39:38 +0900

    feat: 아이콘들 constants에 추가

--- constants/icons.ts
+++ constants/icons.ts
@@ -3,5 +3,23 @@ import GithubIcon from "../assets/icons/github.svg";
 import KakaoIcon from "../assets/icons/kakao.svg";
 import EyeIcon from "../assets/icons/eye.svg";
 import EyeOffIcon from "../assets/icons/eye-off.svg";
+import ArrowLeftIcon from "../assets/icons/arrow_left.svg";
+import ArrowRightIcon from "../assets/icons/arrow_right.svg";
+import FaceDefaultIcon from "../assets/icons/face_default.svg";
+import FaceDoneIcon from "../assets/icons/face_done.svg";
+import FaceNotDoneIcon from "../assets/icons/face_not_done.svg";
+import FaceRestIcon from "../assets/icons/face_rest.svg";
 
-export default { GoogleIcon, GithubIcon, KakaoIcon, EyeIcon, EyeOffIcon };
+export default {
+	GoogleIcon,
+	GithubIcon,
+	KakaoIcon,
+	EyeIcon,
+	EyeOffIcon,
+	ArrowLeftIcon,
+	ArrowRightIcon,
+	FaceDefaultIcon,
+	FaceDoneIcon,
+	FaceNotDoneIcon,
+	FaceRestIcon,
+};
