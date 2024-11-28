7641e7260cb11f9d1db2796be79f4c29f52e978e
CitrusSoda <dndhkaus1004@naver.com>
2024-11-28 20:19:14 +0900

    feat: 상단 헤더 추가 및 회원가입

---
+++ app/(auth)/sign-up/step1.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+const Step1 = () => {
+	return (
+		<View className="h-full bg-white">
+			<Text>step1</Text>
+		</View>
+	);
+};
+
+export default Step1;
--- constants/icons.ts
+++ constants/icons.ts
@@ -3,5 +3,13 @@ import GithubIcon from "../assets/icons/github.svg";
 import KakaoIcon from "../assets/icons/kakao.svg";
 import EyeIcon from "../assets/icons/eye.svg";
 import EyeOffIcon from "../assets/icons/eye-off.svg";
+import ChevronLeftIcon from "../assets/icons/chevron-left.svg";
 
-export default { GoogleIcon, GithubIcon, KakaoIcon, EyeIcon, EyeOffIcon };
+export default {
+	GoogleIcon,
+	GithubIcon,
+	KakaoIcon,
+	EyeIcon,
+	EyeOffIcon,
+	ChevronLeftIcon,
+};
