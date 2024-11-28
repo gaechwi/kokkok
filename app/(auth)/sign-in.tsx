7ad02437d205870b99341958cb005fb8a2650654
CitrusSoda <dndhkaus1004@naver.com>
2024-11-28 15:47:43 +0900

    feat: 간편 로그인 틀 완성

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -6,7 +6,9 @@ import {
 	TextInput,
 	TouchableOpacity,
 } from "react-native";
+
 import images from "../../constants/images";
+import icons from "../../constants/icons";
 
 const SignIn = () => {
 	return (
@@ -36,6 +38,23 @@ const SignIn = () => {
 					<TouchableOpacity className="mt-10 h-[62px] w-full items-center justify-center rounded-[10px] bg-primary">
 						<Text className="heading-2 text-white">로그인</Text>
 					</TouchableOpacity>
+
+					<View className="mt-10 flex flex-row justify-center gap-3">
+						<Text className="body-1 text-gray-50">비밀번호 찾기</Text>
+						<Text className="body-1 text-gray-50">|</Text>
+						<Text className="body-1 text-gray-50">회원가입</Text>
+					</View>
+
+					<View className="mt-14 flex items-center">
+						<View>
+							<Text className="title-3 text-gray-65">간편 로그인</Text>
+						</View>
+						<View className="mt-4 flex-row items-center gap-4">
+							<icons.GithubIcon width={56} height={56} />
+							<icons.KakaoIcon width={56} height={56} />
+							<icons.GoogleIcon width={56} height={56} />
+						</View>
+					</View>
 				</View>
 			</ScrollView>
 		</View>
---
+++ constants/icons.ts
@@ -0,0 +1,5 @@
+import GoogleIcon from "../assets/icons/google.svg";
+import GithubIcon from "../assets/icons/github.svg";
+import KakaoIcon from "../assets/icons/kakao.svg";
+
+export default { GoogleIcon, GithubIcon, KakaoIcon };
