26546fc373a4d66941e9d85ece90c937b363104c
un0211 <un0211@kaist.ac.kr>
2024-11-29 14:38:52 +0900

    feat: 상단 탭 UI 구현

--- app/(tabs)/friend.tsx
+++ app/(tabs)/friend.tsx
@@ -1,9 +1,21 @@
+import { useState } from "react";
+import FriendTab from "@/components/FriendTab";
 import { View, Text } from "react-native";
 
 export default function Friend() {
+  const [isFriendTap, setIsFriendTap] = useState(true);
+
   return (
     <View>
-      <Text>Friend</Text>
+      <FriendTab
+        isFriendTap={isFriendTap}
+        handlePress={(newIsFriendTab: boolean) =>
+          setIsFriendTap(newIsFriendTab)
+        }
+      />
+      <View className="bg-white">
+        <Text>Friend</Text>
+      </View>
     </View>
   );
 }
