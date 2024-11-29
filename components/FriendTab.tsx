b22d687bb79a766a2357b118909239e9a40fc441
un0211 <un0211@kaist.ac.kr>
2024-11-29 15:36:35 +0900

    fix: 상단탭에서  폰트색상 gray-90 적용

--- components/FriendTab.tsx
+++ components/FriendTab.tsx
@@ -15,7 +15,9 @@ export default function FriendTab({
         className={`flex-1 h-full justify-center items-center ${isFriendTap ? "border-b-2 border-primary" : "border-b-[1px] border-gray-20"}`}
         onPress={() => handlePress(true)}
       >
-        <Text className={`title-2 ${isFriendTap ? "" : "font-pmedium"}`}>
+        <Text
+          className={`title-2 text-gray-90 ${isFriendTap ? "" : "font-pmedium"}`}
+        >
           친구 목록
         </Text>
       </TouchableOpacity>
@@ -23,7 +25,9 @@ export default function FriendTab({
         className={`flex-1 h-full justify-center items-center ${isFriendTap ? "border-b-[1px] border-gray-20" : "border-b-2 border-primary"}`}
         onPress={() => handlePress(false)}
       >
-        <Text className={`title-2 ${isFriendTap ? "font-pmedium" : ""}`}>
+        <Text
+          className={`title-2 text-gray-90 ${isFriendTap ? "font-pmedium" : ""}`}
+        >
           친구 요청
         </Text>
       </TouchableOpacity>
