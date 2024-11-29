6a885a34b84d89b08770dd2756b3e4c82b6d0d65
un0211 <un0211@kaist.ac.kr>
2024-11-29 16:23:30 +0900

    refactor: 탭 아이템 컴포넌트화

--- components/FriendTab.tsx
+++ components/FriendTab.tsx
@@ -5,32 +5,39 @@ interface FriendTapProps {
   handlePress: (nIF: boolean) => void;
 }
 
+interface TabItemProps {
+  title: string;
+  handlePress: () => void;
+  isActive: boolean;
+}
+
+const TabItem = ({ title, handlePress, isActive }: TabItemProps) => (
+  <TouchableOpacity
+    className={`flex-1 h-full justify-center items-center ${isActive ? "border-b-2 border-primary" : "border-b-[1px] border-gray-20"}`}
+    onPress={handlePress}
+  >
+    <Text className={`title-2 text-gray-90 ${isActive ? "" : "font-pmedium"}`}>
+      {title}
+    </Text>
+  </TouchableOpacity>
+);
+
 export default function FriendTab({
   isFriendTap,
   handlePress,
 }: FriendTapProps) {
   return (
     <View className="w-full h-[64px] flex-row bg-white">
-      <TouchableOpacity
-        className={`flex-1 h-full justify-center items-center ${isFriendTap ? "border-b-2 border-primary" : "border-b-[1px] border-gray-20"}`}
-        onPress={() => handlePress(true)}
-      >
-        <Text
-          className={`title-2 text-gray-90 ${isFriendTap ? "" : "font-pmedium"}`}
-        >
-          친구 목록
-        </Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        className={`flex-1 h-full justify-center items-center ${isFriendTap ? "border-b-[1px] border-gray-20" : "border-b-2 border-primary"}`}
-        onPress={() => handlePress(false)}
-      >
-        <Text
-          className={`title-2 text-gray-90 ${isFriendTap ? "font-pmedium" : ""}`}
-        >
-          친구 요청
-        </Text>
-      </TouchableOpacity>
+      <TabItem
+        title="친구 목록"
+        handlePress={() => handlePress(true)}
+        isActive={isFriendTap === true}
+      />
+      <TabItem
+        title="친구 요청"
+        handlePress={() => handlePress(false)}
+        isActive={isFriendTap === false}
+      />
     </View>
   );
 }
