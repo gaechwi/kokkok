602feda67d71340c3a740ca8ec9582a60bdd38f9
un0211 <un0211@kaist.ac.kr>
2024-11-30 22:53:24 +0900

    fix: 안 읽은 알림 부분 삭제

--- app/notification.tsx
+++ app/notification.tsx
@@ -1,10 +1,8 @@
-import { ScrollView, Text, View } from "react-native";
+import { ScrollView, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 import { NOTIFICATIONS } from "@/mockData/notification";
 import { NotificationItem } from "@/components/NotificationItem";
-
-const unreadNotification = NOTIFICATIONS;
 
 export default function Notification() {
   return (
@@ -13,26 +11,10 @@ export default function Notification() {
         {/* 상단에 패딩을 주면 일부 모바일에서 패딩만큼 끝이 잘려보여서 높이 조절을 위해 추가 */}
         <View className="h-2" />
 
-        {[1, 2, 3].map((n) =>
+        {[1, 2, 3, 4].map((n) =>
           NOTIFICATIONS.map((notification) => (
             <NotificationItem key={n + notification.id} {...notification} />
           )),
-        )}
-
-        {unreadNotification.length > 0 && (
-          <>
-            <View className="w-full pt-1.5 pb-2 justify-center items-center">
-              <Text className="caption-3 text-gray-45">확인한 알림</Text>
-            </View>
-
-            {unreadNotification.map((notification) => (
-              <NotificationItem
-                key={notification.id}
-                checked
-                {...notification}
-              />
-            ))}
-          </>
         )}
       </ScrollView>
     </SafeAreaView>
