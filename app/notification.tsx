2cf300b920d93bef43091405ae24f58c3358fcc4
un0211 <un0211@kaist.ac.kr>
2024-11-30 21:57:27 +0900

    feat: 알림 페이지 UI 구현

--- app/notification.tsx
+++ app/notification.tsx
@@ -1,1 +1,40 @@
-export default function Notification() {}
+import { ScrollView, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+import { NOTIFICATIONS } from "@/mockData/notification";
+import { NotificationItem } from "@/components/NotificationItem";
+
+const unreadNotification = NOTIFICATIONS;
+
+export default function Notification() {
+  return (
+    <SafeAreaView edges={[]} className="flex-1 bg-white">
+      <ScrollView className="grow w-full">
+        {/* 상단에 패딩을 주면 일부 모바일에서 패딩만큼 끝이 잘려보여서 높이 조절을 위해 추가 */}
+        <View className="h-2" />
+
+        {[1, 2, 3].map((n) =>
+          NOTIFICATIONS.map((notification) => (
+            <NotificationItem key={n + notification.id} {...notification} />
+          )),
+        )}
+
+        {unreadNotification.length > 0 && (
+          <>
+            <View className="w-full pt-1.5 pb-2 justify-center items-center">
+              <Text className="caption-3 text-gray-45">확인한 알림</Text>
+            </View>
+
+            {unreadNotification.map((notification) => (
+              <NotificationItem
+                key={notification.id}
+                checked
+                {...notification}
+              />
+            ))}
+          </>
+        )}
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
--- mockData/notification.ts
+++ mockData/notification.ts
@@ -5,6 +5,7 @@ export interface Notification {
   actor: User;
   type: "POKE" | "COMMENT" | "LIKE";
   postId?: string;
+  comment?: string;
   createdAt: string;
 }
 
@@ -20,6 +21,7 @@ export const NOTIFICATIONS: Notification[] = [
     actor: USERS[1],
     type: "COMMENT",
     postId: "1",
+    comment: "완전 열심히 운동하시네요 맨날 동일한 시간에 하시나요?",
     createdAt: "2024-11-27",
   },
   {
@@ -28,5 +30,13 @@ export const NOTIFICATIONS: Notification[] = [
     type: "LIKE",
     postId: "2",
     createdAt: "2024-10-27",
+  },
+  {
+    id: "4",
+    actor: USERS[0],
+    type: "COMMENT",
+    postId: "2",
+    comment: "오늘도 화아팅!",
+    createdAt: "2023-11-27",
   },
 ];
