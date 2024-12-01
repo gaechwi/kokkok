33340d1bc48fbb131e55b87b5a3ec866536c7707
heony704 <heony704@gmail.com>
2024-12-01 23:29:25 +0900

    feat: 쉬는 날 설정 모달에 CalendarNavigator 추가

--- components/SetRestDayModal.tsx
+++ components/SetRestDayModal.tsx
@@ -1,5 +1,7 @@
 import { Text, TouchableOpacity, View } from "react-native";
+import { useState } from "react";
 import CustomModal from "./Modal";
+import CalendarNavigator from "./CalendarNavigator";
 import icons from "@/constants/icons";
 import colors from "@/constants/colors";
 
@@ -12,6 +14,28 @@ export default function SetRestDayModal({
   visible,
   onClose,
 }: SetRestDayModalProps) {
+  const [date, setDate] = useState<Date>(new Date());
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+
+  const currentDate = new Date();
+  const currentYear = currentDate.getFullYear();
+  const currentMonth = currentDate.getMonth() + 1;
+
+  const isPreviousDisabled = year === currentYear && month <= currentMonth;
+
+  const handlePreviousMonth = () => {
+    const newDate = new Date(date);
+    newDate.setMonth(date.getMonth() - 1);
+    setDate(newDate);
+  };
+
+  const handleNextMonth = () => {
+    const newDate = new Date(date);
+    newDate.setMonth(date.getMonth() + 1);
+    setDate(newDate);
+  };
+
   return (
     <CustomModal visible={visible} onClose={onClose} position="bottom">
       <View className="relative items-center px-[39px] pt-[40px] pb-[52px]">
@@ -21,10 +45,21 @@ export default function SetRestDayModal({
           height={24}
           color={colors.gray[90]}
         />
-        <Text className="heading-1">쉬는 날을 설정하세요.</Text>
-        <Text>
-          {"<"} 11월 {">"}
-        </Text>
+
+        <Text className="heading-1">쉬는 날을 설정하세요</Text>
+
+        <CalendarNavigator className="mt-[21px] gap-[16px]">
+          <CalendarNavigator.PreviousButton
+            onPress={handlePreviousMonth}
+            disabled={isPreviousDisabled}
+          />
+          <CalendarNavigator.MonthDisplay
+            className="text-gray-40"
+            date={date}
+          />
+          <CalendarNavigator.NextButton onPress={handleNextMonth} />
+        </CalendarNavigator>
+
         <Text>일월화수목금토</Text>
         <Text>12</Text>
         <Text>3456789</Text>
