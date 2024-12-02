32f0ea1888618d43a1cc6bcaa283088a405ee8f6
CitrusSoda <dndhkaus1004@naver.com>
2024-12-02 13:48:52 +0900

    feat: 로그인 ui

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -55,7 +55,7 @@ const SignIn = () => {
 
           <View className="mt-10 flex w-full gap-8">
             <TextInput
-              className="h-[58px] w-full rounded-[10px] border border-gray-20 px-4 focus:border-primary"
+              className="placeholder:body-1 h-[58px] w-full rounded-[10px] border border-gray-20 px-4 focus:border-primary"
               placeholder="이메일을 입력해주세요."
               keyboardType="email-address"
               autoCapitalize="none"
@@ -68,7 +68,7 @@ const SignIn = () => {
             />
             <View className="w-full">
               <TextInput
-                className="h-[58px] w-full rounded-[10px] border border-gray-20 px-4 focus:border-primary"
+                className="placeholder:body-1 h-[58px] w-full rounded-[10px] border border-gray-20 px-4 focus:border-primary"
                 placeholder="비밀번호를 입력해주세요."
                 secureTextEntry={!showPassword}
                 accessibilityLabel="비밀번호 입력"
@@ -115,9 +115,9 @@ const SignIn = () => {
               <Text className="title-3 text-gray-65">간편 로그인</Text>
             </View>
             <View className="mt-4 flex-row items-center gap-4">
-              <icons.GithubIcon width={56} height={56} />
-              <icons.KakaoIcon width={56} height={56} />
+              <icons.AppleIcon width={56} height={56} />
               <icons.GoogleIcon width={56} height={56} />
+              <icons.GithubIcon width={56} height={56} />
             </View>
           </View>
         </View>
