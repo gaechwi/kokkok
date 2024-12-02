575ed68cd0bfd79304a089c332f608bca1d221ff
정민재 <89517903+wjsdncl@users.noreply.github.com>
2024-12-02 15:24:14 +0900

    feat: 게시글 목록 조회 기능 임시 추가 (#28)
    * feat: 게시글 목록 조회 기능 추가
    
    * feat: 게시글 목록 무한스크롤 기능 추가
    
    * fix: 게시글 목록 API 에러 핸들링 및 게시글 내용 자르기 수정
    
    * fix: 게시글 목록 무한스크롤 최적화
    
    * fix: supabase로 변경

--- components/PostItem.tsx
+++ components/PostItem.tsx
@@ -11,13 +11,14 @@ import { diffDate } from "@/utils/formatDate";
 import { useMemo, useState } from "react";
 import icons from "@/constants/icons";
 import CustomModal from "./Modal";
+import colors from "@/constants/colors";
 interface PostItemProps {
   author: {
     name: string;
     avatar: string;
   };
   images: string[];
-  content?: string;
+  contents?: string;
   liked: boolean;
   likedAuthorAvatar?: string[];
   createdAt: string;
@@ -34,7 +35,7 @@ interface PostItemProps {
 export default function PostItem({
   author,
   images,
-  content,
+  contents,
   liked,
   likedAuthorAvatar,
   createdAt,
@@ -50,7 +51,7 @@ export default function PostItem({
 
   const calculateMaxChars = useMemo(() => {
     const fontScale = PixelRatio.getFontScale();
-    const baseCharsPerLine = Math.floor(screenWidth / (11 * fontScale));
+    const baseCharsPerLine = Math.floor(screenWidth / (15 * fontScale));
     return baseCharsPerLine * 2;
   }, [screenWidth]);
 
@@ -58,14 +59,20 @@ export default function PostItem({
     if (!text || text.length <= calculateMaxChars) return text;
     const truncated = text.slice(0, calculateMaxChars);
     const lastSentence = truncated.match(/[^.!?]*[.!?]+/g);
+
+    let result: string;
     if (lastSentence && lastSentence.length > 0) {
-      return truncated.slice(
+      result = truncated.slice(
         0,
         truncated.lastIndexOf(lastSentence[lastSentence.length - 1]) + 1,
       );
+    } else {
+      const lastSpace = truncated.lastIndexOf(" ");
+      result = lastSpace > 0 ? truncated.slice(0, lastSpace) : truncated;
     }
-    const lastSpace = truncated.lastIndexOf(" ");
-    return lastSpace > 0 ? truncated.slice(0, lastSpace) : truncated;
+
+    result = result.replace(/\s$/, "");
+    return `${result}...`;
   };
 
   const onOpenModal = () => {
@@ -78,128 +85,137 @@ export default function PostItem({
 
   return (
     <View className="grow bg-gray-10 pb-[10px]">
-      {/* header */}
-      <View className="flex-row items-center justify-between bg-white px-4">
-        <TouchableOpacity>
-          <View className="h-14 flex-row items-center gap-4">
-            <Image
-              source={{ uri: author.avatar }}
-              resizeMode="cover"
-              className="size-8 rounded-full"
-            />
-            <Text className="body-3 text-gray-80">{author.name}</Text>
-          </View>
-        </TouchableOpacity>
-
-        <TouchableOpacity onPress={onOpenModal}>
-          <icons.MeatballIcon width={24} height={24} color="#5D5D5D" />
-
-          <CustomModal
-            visible={isModalVisible}
-            onClose={onCloseModal}
-            position="bottom"
-          >
-            <View className="items-center">
-              <TouchableOpacity className="h-[82px] w-full items-center justify-center border-gray-20 border-b">
-                <Text className="title-2 text-gray-90">수정하기</Text>
-              </TouchableOpacity>
-              <TouchableOpacity className="h-[82px] w-full items-center justify-center">
-                <Text className="title-2 text-gray-90">삭제하기</Text>
-              </TouchableOpacity>
+      <View className="grow bg-white ">
+        {/* header */}
+        <View className="flex-row items-center justify-between bg-white px-4">
+          <TouchableOpacity>
+            <View className="h-14 flex-row items-center gap-4">
+              <Image
+                source={{ uri: author.avatar }}
+                resizeMode="cover"
+                className="size-8 rounded-full"
+              />
+              <Text className="body-3 text-gray-80">{author.name}</Text>
             </View>
-          </CustomModal>
-        </TouchableOpacity>
-      </View>
-
-      {/* carousel */}
-      <View className="h-max w-full bg-white pb-1">
-        <Carousel images={images} />
-      </View>
-
-      {/* relation */}
-      <View className="flex-row items-center justify-between bg-white px-4 pb-4">
-        <View className="flex-row items-center gap-2 pr-[2px]">
-          <TouchableOpacity onPress={() => setIsLiked(!isLiked)}>
-            <icons.HeartIcon
-              width={24}
-              height={24}
-              color={isLiked ? "#ff7a7a" : "#333333"}
-              fill={isLiked ? "#ff7a7a" : "transparent"}
-            />
           </TouchableOpacity>
-          {likedAuthorAvatar && likedAuthorAvatar.length > 0 && (
-            <TouchableOpacity className="flex-row items-center">
-              {likedAuthorAvatar.slice(0, 2).map((avatar, index) => (
-                <Image
-                  // biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
-                  key={`avatar-${index}`}
-                  source={{ uri: avatar }}
-                  resizeMode="cover"
-                  className="-ml-[5px] size-[24px] rounded-full"
-                  style={{
-                    zIndex: 5 - index,
-                    borderWidth: 1,
-                    borderColor: "white",
-                  }}
-                />
-              ))}
-              {likedAuthorAvatar.length > 2 && (
-                <Text className="pl-[2px] font-pbold text-[13px] text-gray-90 leading-[150%]">
-                  외 여러명
+
+          <TouchableOpacity onPress={onOpenModal}>
+            <icons.MeatballIcon width={24} height={24} color="#5D5D5D" />
+
+            <CustomModal
+              visible={isModalVisible}
+              onClose={onCloseModal}
+              position="bottom"
+            >
+              <View className="items-center">
+                <TouchableOpacity className="h-[82px] w-full items-center justify-center border-gray-20 border-b">
+                  <Text className="title-2 text-gray-90">수정하기</Text>
+                </TouchableOpacity>
+                <TouchableOpacity className="h-[82px] w-full items-center justify-center">
+                  <Text className="title-2 text-gray-90">삭제하기</Text>
+                </TouchableOpacity>
+              </View>
+            </CustomModal>
+          </TouchableOpacity>
+        </View>
+
+        {/* carousel */}
+        <View className="h-max w-full bg-white pb-1">
+          <Carousel images={images} />
+        </View>
+
+        {/* relation */}
+        <View className="flex-row items-center justify-between bg-white px-4 pb-6">
+          <View className="flex-row items-center pr-[2px]">
+            {/* like */}
+            <TouchableOpacity onPress={() => setIsLiked(!isLiked)}>
+              <icons.HeartIcon
+                width={24}
+                height={24}
+                color={isLiked ? colors.secondary.red : colors.gray[90]}
+                fill={isLiked ? colors.secondary.red : "transparent"}
+              />
+            </TouchableOpacity>
+
+            {/* likeAvatar */}
+            {likedAuthorAvatar && likedAuthorAvatar.length > 0 && (
+              <TouchableOpacity className="ml-[2px] flex-row items-center">
+                {likedAuthorAvatar.slice(0, 2).map((avatar, index) => (
+                  <Image
+                    // biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
+                    key={`avatar-${index}`}
+                    source={{ uri: avatar }}
+                    resizeMode="cover"
+                    className={`size-[24px] rounded-full ${index !== 0 ? "-ml-[9px]" : ""}`}
+                    style={{
+                      zIndex: 5 - index,
+                      borderWidth: 1,
+                      borderColor: "white",
+                    }}
+                  />
+                ))}
+                {likedAuthorAvatar.length > 2 && (
+                  <Text className="pl-[2px] font-pbold text-[13px] text-gray-90 leading-[150%]">
+                    외 여러명
+                  </Text>
+                )}
+              </TouchableOpacity>
+            )}
+
+            {/* comments */}
+            <TouchableOpacity className="ml-[10px] flex-row items-center gap-[4px]">
+              <icons.CommentIcon width={24} height={24} color="#333333" />
+              {commentsCount > 0 && (
+                <Text className="font-pbold text-[13px] text-gray-90 leading-[150%]">
+                  {commentsCount > 99 ? "99+" : commentsCount}
                 </Text>
               )}
             </TouchableOpacity>
+          </View>
+
+          {/* createdAt */}
+          <Text className="font-pmedium text-[10px] text-gray-50 leading-[150%]">
+            {diff}
+          </Text>
+        </View>
+
+        <View className="pb-[22px]">
+          {/* content */}
+          {contents && (
+            <View className="bg-white px-4">
+              <View className="flex-row flex-wrap">
+                <Text className="title-5 text-gray-90">
+                  {isMore ? contents : truncateText(contents)}
+                  {contents.length > calculateMaxChars && (
+                    <TouchableOpacity
+                      onPress={() => setIsMore(!isMore)}
+                      className="h-[16px] flex-row items-center justify-center"
+                    >
+                      <Text className="title-5 -mb-[3px] text-gray-45">
+                        {isMore ? " 접기" : "더보기"}
+                      </Text>
+                    </TouchableOpacity>
+                  )}
+                </Text>
+              </View>
+            </View>
           )}
-          <TouchableOpacity className="flex-row items-center gap-[2px]">
-            <icons.CommentIcon width={24} height={24} color="#333333" />
-            {commentsCount > 0 && (
-              <Text className="caption-1 text-gray-90">
-                {commentsCount > 99 ? "99+" : commentsCount}
-              </Text>
-            )}
-          </TouchableOpacity>
-        </View>
 
-        <Text className="caption-3 text-gray-50">{diff}</Text>
+          {/* comments */}
+          {comment && (
+            <View className="bg-white px-6">
+              <View className="flex-row items-center gap-2 pt-2">
+                <Text className="text-nowrap font-pbold text-[15px] text-gray-70 leading-[150%]">
+                  {comment.author.name}
+                </Text>
+                <Text className="body-2 flex-1 text-gray-90" numberOfLines={1}>
+                  {comment.content}
+                </Text>
+              </View>
+            </View>
+          )}
+        </View>
       </View>
-
-      {/* content */}
-      {content && (
-        <View className="bg-white px-4 pb-4">
-          <View className="flex-row flex-wrap">
-            <Text className="body-2 text-gray-90">
-              {isMore ? content : truncateText(content)}
-              {content.length > calculateMaxChars && (
-                <Text>{!isMore && "..."}</Text>
-              )}
-              {content.length > calculateMaxChars && (
-                <TouchableOpacity
-                  onPress={() => setIsMore(!isMore)}
-                  className="flex-row items-start justify-center"
-                >
-                  <Text className="body-2 -mt-[10px] h-[17px] text-gray-45">
-                    {isMore ? "  접기" : "더보기"}
-                  </Text>
-                </TouchableOpacity>
-              )}
-            </Text>
-          </View>
-        </View>
-      )}
-
-      {/* comments */}
-      {comment && (
-        <View className="bg-white px-6 pb-4">
-          <View className="flex-row items-center gap-2">
-            <Text className="text-nowrap font-pbold text-[15px] text-gray-70 leading-[150%]">
-              {comment.author.name}
-            </Text>
-            <Text className="body-2 flex-1 text-gray-90" numberOfLines={1}>
-              {comment.content}
-            </Text>
-          </View>
-        </View>
-      )}
     </View>
   );
 }
--- utils/supabase.ts
+++ utils/supabase.ts
@@ -78,10 +78,7 @@ export async function signUp({
 export async function signIn({
   email,
   password,
-}: {
-  email: string;
-  password: string;
-}) {
+}: { email: string; password: string }) {
   try {
     const { data, error } = await supabase.auth.signInWithPassword({
       email,
@@ -152,10 +149,7 @@ export async function uploadImage(file: ImagePicker.ImagePickerAsset) {
 export async function createPost({
   contents,
   images,
-}: {
-  contents?: string;
-  images: ImagePicker.ImagePickerAsset[];
-}) {
+}: { contents?: string; images: ImagePicker.ImagePickerAsset[] }) {
   try {
     // 현재 로그인된 사용자 정보 가져오기
     // const {
@@ -200,4 +194,79 @@ export async function createPost({
         : "게시물 생성에 실패했습니다.";
     throw new Error(errorMessage);
   }
+}
+
+// 게시글 조회
+export async function getPosts({
+  offset = 0,
+  limit = 10,
+}: {
+  offset?: number;
+  limit?: number;
+}): Promise<{ posts: Post[]; total: number; hasMore: boolean }> {
+  try {
+    const {
+      data: posts,
+      error: postsError,
+      count,
+    } = await supabase
+      .from("post")
+      .select(
+        `
+        id,
+        images,
+        contents,
+        created_at,
+        likes
+      `,
+        { count: "exact" },
+      )
+      .order("created_at", { ascending: false })
+      .range(offset, offset + limit - 1);
+
+    if (postsError) throw postsError;
+    if (!posts) throw new Error("게시글을 불러올 수 없습니다.");
+
+    return {
+      posts: posts.map(
+        (post): Post => ({
+          id: post.id,
+          images: post.images,
+          contents: post.contents,
+          createdAt: post.created_at,
+          likes: post.likes,
+        }),
+      ),
+      total: count || 0,
+      hasMore: count ? offset + limit < count : false,
+    };
+  } catch (error) {
+    const errorMessage =
+      error instanceof Error ? error.message : "게시글 조회에 실패했습니다";
+    throw new Error(errorMessage);
+  }
+}
+
+// ============================================
+//
+//                    type
+//
+// ============================================
+
+// 게시글 타입 정의
+interface Post {
+  id: string;
+  images: string[];
+  contents: string;
+  createdAt: string;
+  likes: number;
+  // author: User;
+}
+
+// 유저 타입 정의
+interface User {
+  id: string;
+  email: string;
+  username: string;
+  avatarUrl: string;
 }
