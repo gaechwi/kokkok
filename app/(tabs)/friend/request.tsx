4b2ef30315908f5debce4d2ca4474d806ec8c2f3
un0211 <un0211@kaist.ac.kr>
2024-12-04 21:01:45 +0900

    feat: 새로운 친구요청 오면 실시간 반영

--- app/(tabs)/friend/request.tsx
+++ app/(tabs)/friend/request.tsx
@@ -1,17 +1,21 @@
 import { View, FlatList } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { useEffect } from "react";
 
 import { FriendRequest } from "@/components/FriendItem";
-import { getFriendRequests } from "@/utils/supabase";
+import { getFriendRequests, supabase } from "@/utils/supabase";
 import useFetchData from "@/hooks/useFetchData";
 import type { RequestResponse } from "@/types/Friend.interface";
 import ErrorScreen from "@/components/ErrorScreen";
 import LoadingScreen from "@/components/LoadingScreen";
+import { useQueryClient } from "@tanstack/react-query";
 
 const OFFSET = 0;
 const LIMIT = 12;
 
 export default function Request() {
+  const queryClient = useQueryClient();
+
   const {
     data: requests,
     isLoading,
@@ -21,6 +25,26 @@ export default function Request() {
     () => getFriendRequests({ offset: OFFSET, limit: LIMIT }),
     "친구 요청 조회에 실패했습니다.",
   );
+
+  useEffect(() => {
+    const userId = "8a49fc55-8604-4e9a-9c7d-b33a813f3344";
+
+    const requestChannel = supabase
+      .channel("friendRequest")
+      .on(
+        "postgres_changes",
+        { event: "INSERT", schema: "public", table: "friendRequest" },
+        (payload) => {
+          if (!payload.new.isAccepted && payload.new.to === userId)
+            queryClient.invalidateQueries({ queryKey: ["friendRequests"] });
+        },
+      )
+      .subscribe();
+
+    return () => {
+      supabase.removeChannel(requestChannel);
+    };
+  }, [queryClient.invalidateQueries]);
 
   if (error) {
     return (
