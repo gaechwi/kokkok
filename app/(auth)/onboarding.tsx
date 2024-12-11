9987313c30bd9bb8e4d48f81575e2449cd185980
heony704 <heony704@gmail.com>
2024-12-11 22:41:50 +0900

    feat: 온보딩 페이지 추가

---
+++ app/(auth)/onboarding.tsx
@@ -0,0 +1,197 @@
+import { useRef, useState } from "react";
+import {
+  Dimensions,
+  FlatList,
+  ImageBackground,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+import images from "@/constants/images";
+import Animated, {
+  Easing,
+  useAnimatedStyle,
+  withTiming,
+} from "react-native-reanimated";
+
+const { width } = Dimensions.get("window");
+
+const slides = [
+  {
+    id: "slide1",
+    component: <Slide1 />,
+    buttonText: "응! 같이하자",
+  },
+  { id: "slide2", component: <Slide2 /> },
+  { id: "slide3", component: <Slide3 /> },
+  {
+    id: "slide4",
+    component: <Slide4 />,
+    buttonText: "시작하기!",
+  },
+];
+
+export default function Onboarding() {
+  const flatListRef = useRef<FlatList>(null);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handleNext = () => {
+    if (currentIndex < slides.length - 1) {
+      flatListRef.current?.scrollToIndex({ index: currentIndex + 1 });
+      setCurrentIndex((prev) => prev + 1);
+    } else {
+      console.log("온보딩 완료");
+    }
+  };
+
+  return (
+    <SafeAreaView className="flex-1 bg-white">
+      <View className="relative flex-1">
+        <FlatList
+          ref={flatListRef}
+          data={slides}
+          horizontal
+          pagingEnabled
+          scrollEnabled={false}
+          showsHorizontalScrollIndicator={false}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => (
+            <View style={{ width }}>{item.component}</View>
+          )}
+        />
+
+        <View className="absolute bottom-[40px] w-full items-center">
+          <View className="flex-row justify-center">
+            {slides.map((slide, index) => (
+              <Dot key={slide.id} isActive={index === currentIndex} />
+            ))}
+          </View>
+
+          <TouchableOpacity
+            className="mx-[24px] mt-[32px] w-full max-w-[328px] items-center rounded-[10px] bg-primary py-[16px] text-white"
+            onPress={handleNext}
+          >
+            <Text className="heading-2 text-white">
+              {slides[currentIndex].buttonText ?? "다음"}
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+function Slide1() {
+  return (
+    <ImageBackground
+      source={images.OnBoarding1}
+      className="relative flex-1 items-center bg-yellow-100"
+    >
+      <Text className="absolute top-[177px] font-pbold text-[40px] text-gray-90">
+        같이 운동하실래요?
+      </Text>
+      <Text className="absolute bottom-[186px] font-psemibold text-[17px] text-gray-50">
+        콕콕의 사용법에 대해 알아봐요
+      </Text>
+    </ImageBackground>
+  );
+}
+
+function Slide2() {
+  return (
+    <ImageBackground
+      source={images.OnBoarding2}
+      className="relative flex-1 items-center bg-emerald-100"
+    >
+      <View className="absolute top-[130px] items-center gap-[6px]">
+        <Text className="font-pbold text-[40px] text-gray-90">
+          운동이 끝나면
+        </Text>
+        <Text className="font-pbold text-[40px] text-gray-90">
+          사진으로 인증해요
+        </Text>
+      </View>
+    </ImageBackground>
+  );
+}
+
+function Slide3() {
+  return (
+    <ImageBackground
+      source={images.OnBoarding3}
+      className="relative flex-1 items-center bg-sky-100"
+    >
+      <View className="absolute top-[89px] right-[59px] rotate-[8.5deg]">
+        <Text className="text-[110px] text-primary">콕!</Text>
+      </View>
+      <View className="absolute bottom-[204px] items-center gap-[6px]">
+        <Text className="font-pbold text-[40px] text-gray-90">운동 안 한</Text>
+        <Text className="font-pbold text-[40px] text-gray-90">
+          친구를 찔러요
+        </Text>
+      </View>
+    </ImageBackground>
+  );
+}
+
+function Slide4() {
+  return (
+    <ImageBackground
+      source={images.OnBoarding4}
+      className="relative flex-1 bg-rose-100"
+    >
+      <View className="absolute top-[95px] left-[60px] gap-[6px]">
+        <Text className="font-pbold text-[40px] text-gray-90">
+          귀여운 이모지로
+        </Text>
+        <Text className="ml-[30px] font-pbold text-[40px] text-gray-90">
+          한눈에
+        </Text>
+      </View>
+      <View className="absolute right-[60px] bottom-[187px] items-end gap-[6px]">
+        <Text className="font-pbold text-[40px] text-gray-90 ">내 기록을</Text>
+        <Text className="mr-[30px] font-pbold text-[40px] text-gray-90">
+          확인해요
+        </Text>
+      </View>
+    </ImageBackground>
+  );
+}
+
+interface DotProps {
+  isActive: boolean;
+  activeWidth?: number;
+  inactiveWidth?: number;
+  height?: number;
+  activeColor?: string;
+  inactiveColor?: string;
+}
+
+const COLORS = {
+  ACTIVE: "#8356F5",
+  INACTIVE: "#BEBEBE",
+};
+
+const Dot = ({
+  isActive,
+  activeWidth = 24,
+  inactiveWidth = 8,
+  height = 8,
+  activeColor = COLORS.ACTIVE,
+  inactiveColor = COLORS.INACTIVE,
+}: DotProps) => {
+  const animatedStyle = useAnimatedStyle(() => ({
+    width: withTiming(isActive ? activeWidth : inactiveWidth, {
+      duration: 400,
+      easing: Easing.out(Easing.quad),
+    }),
+    height,
+    borderRadius: 4,
+    marginHorizontal: 4,
+    backgroundColor: isActive ? activeColor : inactiveColor,
+  }));
+
+  return <Animated.View style={animatedStyle} />;
+};
