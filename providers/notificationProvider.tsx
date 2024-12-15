5c5b032f577ad76ca57fd0989a705a234d95713f
un0211 <un0211@kaist.ac.kr>
2024-12-15 21:52:32 +0900

    feat: 앱에 다시 접속했을 때, 권한설정 변경했는지 확인하고 정보 업데이트

--- providers/notificationProvider.tsx
+++ providers/notificationProvider.tsx
@@ -1,12 +1,17 @@
 import useFetchData from "@/hooks/useFetchData";
 import type { PushSetting } from "@/types/Notification.interface";
 import { addPushToken, updatePushToken } from "@/utils/pushTokenManager";
-import { getCurrentSession, getPushSetting } from "@/utils/supabase";
+import {
+  getCurrentSession,
+  getPushSetting,
+  updatePushSetting,
+} from "@/utils/supabase";
 import type { Session } from "@supabase/supabase-js";
 import { useQueryClient } from "@tanstack/react-query";
 import * as Notifications from "expo-notifications";
 import { router } from "expo-router";
-import { useCallback, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
+import { AppState } from "react-native";
 
 Notifications.setNotificationHandler({
   handleNotification: async () => ({
@@ -23,6 +28,7 @@ interface Props {
 export default function NotificationProvider({ children }: Props) {
   const queryClient = useQueryClient();
   const [isInit, setIsInit] = useState(true);
+  const [pushPermission, setPushPermission] = useState("");
 
   // 로그인한 유저 정보 조회
   const { data: session } = useFetchData<Session>(
@@ -40,23 +46,23 @@ export default function NotificationProvider({ children }: Props) {
       !!session,
     );
 
-  // 푸시알람 관련 정보 업데이트 시 캐시된 데이터 삭제, 더이상 첫 업데이트 아님을 마킹
-  const handleUpdate = useCallback(() => {
-    queryClient.invalidateQueries({ queryKey: ["pushToken"] });
-    queryClient.invalidateQueries({ queryKey: ["pushTokenSetting"] });
-    setIsInit(false);
-  }, [queryClient]);
-
   // 세션 바뀔 때마다 isInit true로 바꿈
   useEffect(() => {
     if (!session) return;
     setIsInit(true);
   }, [session]);
 
-  // session과 token 유효하고, 로그인 한 첫회에만 푸시 토큰 업데이트
+  // 로그인 한 첫회에만 푸시 토큰 업데이트
   useEffect(() => {
     if (!session || isTokenPending) return;
     if (!isInit) return;
+
+    // 푸시알람 관련 정보 업데이트 시 캐시된 데이터 삭제, 더이상 첫 업데이트 아님을 마킹
+    const handleUpdate = () => {
+      queryClient.invalidateQueries({ queryKey: ["pushToken"] });
+      queryClient.invalidateQueries({ queryKey: ["pushTokenSetting"] });
+      setIsInit(false);
+    };
 
     const userId = session.user.id;
     if (pushSetting) {
@@ -68,7 +74,7 @@ export default function NotificationProvider({ children }: Props) {
     } else {
       addPushToken({ userId, handleUpdate });
     }
-  }, [session, pushSetting, isTokenPending, isInit, handleUpdate]);
+  }, [session, pushSetting, isTokenPending, isInit, queryClient]);
 
   // 푸시 알림 관련 포스트 페이지로 바로 이동
   useEffect(() => {
@@ -87,6 +93,41 @@ export default function NotificationProvider({ children }: Props) {
 
     return () => subscription.remove();
   }, []);
+
+  // 권한 설정 변경 감지
+  useEffect(() => {
+    // 앱 푸시알림 설정 변경 시 관련 정보 리패치
+    const handleUpdate = () => {
+      queryClient.refetchQueries({ queryKey: ["pushToken"] });
+      queryClient.refetchQueries({ queryKey: ["pushTokenSetting"] });
+    };
+
+    // 권한 설정 정보 저장
+    const handlePermissionChange = async () => {
+      const { status } = await Notifications.getPermissionsAsync();
+      if (status === pushPermission || !session) return;
+
+      const userId = session.user.id;
+      if (status === "granted") {
+        await updatePushToken({ userId, existingToken: null, handleUpdate });
+      } else {
+        await updatePushSetting({ userId, token: null });
+        handleUpdate();
+      }
+
+      setPushPermission(status);
+    };
+
+    // 앱이 foreground 로 돌아왔을 때 권한변경 감지
+    const subscription = AppState.addEventListener("change", (nextAppState) => {
+      if (nextAppState === "active") {
+        console.log("welcome back");
+        handlePermissionChange();
+      }
+    });
+
+    return () => subscription.remove();
+  }, [pushPermission, session, queryClient]);
 
   return children;
 }
