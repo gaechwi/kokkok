0eb736980608d4b785c9301b59c4c577d1063db4
un0211 <un0211@kaist.ac.kr>
2024-12-20 11:59:07 +0900

    fix: 알림 시간 업데이트 후 쿼리 무효화

--- app/(protected)/notification.tsx
+++ app/(protected)/notification.tsx
@@ -26,10 +26,14 @@ export default function Notification() {
 
   useFocusEffect(() => {
     // 알림 페이지 방문 시간 업데이트하고, 그에 따라 유저 알림 정보 다시 가져오도록 함
-    updateNotificationCheck();
-    queryClient.invalidateQueries({
-      queryKey: ["notificationCheckedAt"],
-    });
+    const handleUpdate = async () => {
+      await updateNotificationCheck();
+      queryClient.invalidateQueries({
+        queryKey: ["notificationCheckedAt"],
+      });
+    };
+
+    handleUpdate();
   });
 
   if (error) {
