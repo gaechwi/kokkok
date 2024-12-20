5fde17d44172e0d6925fe7ec8d04d1630f36663a
un0211 <un0211@kaist.ac.kr>
2024-12-20 16:37:32 +0900

    fix: useAuthSession 간단한 에러처리

--- hooks/useAuthSession.ts
+++ hooks/useAuthSession.ts
@@ -5,16 +5,21 @@ import * as SecureStore from "expo-secure-store";
 import { useEffect, useState } from "react";
 
 async function updateUserInfo(session: Session | null) {
-  if (session)
-    await Promise.all([
-      SecureStore.setItemAsync("userId", session.user.id),
-      SecureStore.setItemAsync("createdAt", session.user.created_at),
-    ]);
-  else
-    await Promise.all([
-      SecureStore.deleteItemAsync("userId"),
-      SecureStore.deleteItemAsync("createdAt"),
-    ]);
+  try {
+    if (session)
+      await Promise.all([
+        SecureStore.setItemAsync("userId", session.user.id),
+        SecureStore.setItemAsync("createdAt", session.user.created_at),
+      ]);
+    else
+      await Promise.all([
+        SecureStore.deleteItemAsync("userId"),
+        SecureStore.deleteItemAsync("createdAt"),
+      ]);
+  } catch (error) {
+    console.error("유저 정보를 스토리지에 저장하는 중 에러 발생", error);
+    throw error;
+  }
 }
 
 export function useAuthSession(queryClient: QueryClient) {
@@ -22,20 +27,32 @@ export function useAuthSession(queryClient: QueryClient) {
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    supabase.auth.getSession().then(async ({ data: { session } }) => {
-      setIsLoggedIn(!!session);
-      await updateUserInfo(session);
-      setIsLoading(false);
-    });
+    supabase.auth
+      .getSession()
+      .then(async ({ data: { session } }) => {
+        setIsLoggedIn(!!session);
+        await updateUserInfo(session);
+        setIsLoading(false);
+      })
+      .catch(async (error) => {
+        console.error("세션 조회 및 저장 중 오류 발생:", error);
+        setIsLoggedIn(false);
+        await updateUserInfo(null);
+        setIsLoading(false);
+      });
   }, []);
 
   useEffect(() => {
     const {
       data: { subscription },
     } = supabase.auth.onAuthStateChange(async (_event, session) => {
-      setIsLoggedIn(!!session);
-      await updateUserInfo(session);
-      queryClient.clear();
+      try {
+        setIsLoggedIn(!!session);
+        await updateUserInfo(session);
+        queryClient.clear();
+      } catch (error) {
+        console.error("인증 상태 업데이트 중 오류 발생:", session, error);
+      }
     });
 
     return () => {
